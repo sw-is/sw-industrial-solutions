106edd877b4f81df83fc1cee1cc2d49c76c261ff
t-tiana <t.tiana@keemail.me>
2023-10-22 12:03:20 +0200

    buttonSystem+swipeActions

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,87 +1,100 @@
-import React, {useEffect, useState} from 'react';
-import styles from "./menu.module.scss";
+import React, { useEffect, useState } from 'react';
+import styles from './menu.module.scss';
 import LocaleSwitch from '../LocaleSwitch/LocaleSwitch';
+import { useSwipeable } from 'react-swipeable';
 
-function Menu() {
+function Menu () {
 
+	const [viewport, setViewport] = useState(0);
+	const [mobileMenu, setMobileMenu] = useState(false);
 
-  const [viewport, setViewport] = useState(0);
-  const [mobileMenu, setMobileMenu] = useState(false);
-  useEffect(()=>{
-    setViewport(window.innerWidth)
-    function handleViewportResize(){
-      setViewport(window.innerWidth)
-    }
-    window.addEventListener('resize', handleViewportResize);
-    return()=>{
-      window.removeEventListener('resize', handleViewportResize)
-    }
-
-  },[])
-  const mobileDevice = viewport <= 799;
+	const toggleMobileMenu = ()=> {
+		setMobileMenu(!mobileMenu)
+	}
 
 
-  return (
-    mobileDevice ?
-      <>
-        <div className={styles['menu']}>
-          <div className={styles['o-content']}>
-            <img className={styles['m-logo']}
-                 alt=""
-                 src="/sw-logo-color.svg"
-            />
-        
-              <button className={styles['a-button']} onClick={(()=> setMobileMenu(!mobileMenu))}>
-                {!mobileMenu ? 'Menu'  : 'Close' }
-              </button>
-          </div>
-
-          {mobileMenu ?
+	const swipeActions = useSwipeable({
+		onSwipeStart: () => toggleMobileMenu(),
+		onSwiped: () => toggleMobileMenu(),
+	});
 
 
-         <div className={styles['o-mobileContent']}>
-           <button className={styles['a-button']}>
-            About
-            </button>
-            <button className={styles['a-button']}>
-            Services
-          </button>
-          <button className={styles['m-contactUsButton']}>
-            Contact Us
-          </button>
-          <LocaleSwitch/>
-        </div>
 
-: null }
-        </div>
+	useEffect(() => {
+		setViewport(window.innerWidth);
 
-      </>
-      :
-      <div className={styles['menu']}
-      >
-        <div className={styles['o-content']}>
-          <img className={styles['m-logo']} alt="" src="/sw-logo-color.svg" />
-            <div className={styles['m-navigationBlock']}>
-            <button className={styles['a-button']}>
-            About
-            </button>
-            <button className={styles['a-button']}>
-            Services
-          </button>
-          <button className={styles['a-button']}>
-            Contact
-          </button>
-        </div>
-        <div className={styles['m-leftBlock']}>
-          <LocaleSwitch/>
-          <button className={styles['m-contactUsButton']}>
-            Contact Us
-          </button>
-        </div>
-      </div>
-</div>
+		function handleViewportResize () {
+			setViewport(window.innerWidth);
+		}
 
-);
+		window.addEventListener('resize', handleViewportResize);
+		return () => {
+			window.removeEventListener('resize', handleViewportResize);
+		};
+
+	}, []);
+	const mobileDevice = viewport <= 799;
+
+	return (
+		mobileDevice ?
+			<>
+				<div className={styles['menu']}>
+					<div className={styles['o-content']}>
+						<img className={styles['m-logo']}
+								 alt=""
+								 src="/sw-logo-color.svg"
+						/>
+
+						<button className={styles['a-button']} onClick={toggleMobileMenu}>
+							{!mobileMenu ? 'Menu' : 'Close'}
+						</button>
+					</div>
+
+					{mobileMenu ?
+            <div className={styles['o-mobileMenu']} {...swipeActions}>
+						<div className={styles['m-navigationBlock']}>
+							<button className={styles['a-button']}>
+								About
+							</button>
+							<button className={styles['a-button']}>
+								Services
+							</button>
+							<button className={styles['a-contactUsButton']}>
+								Contact Us
+							</button>
+							<LocaleSwitch />
+						</div>
+						</div>
+						: null}
+				</div>
+
+			</>
+			:
+			<div className={styles['menu']}
+			>
+				<div className={styles['o-content']}>
+					<img className={styles['m-logo']} alt="" src="/sw-logo-color.svg" />
+					<div className={styles['m-navigationBlock']}>
+						<button className={styles['a-button']}>
+							About
+						</button>
+						<button className={styles['a-button']}>
+							Services
+						</button>
+						<button className={styles['a-button']}>
+							Contact
+						</button>
+					</div>
+					<div className={styles['m-leftBlock']}>
+						<LocaleSwitch />
+						<button className={styles['a-contactUsButton']}>
+							Contact Us
+						</button>
+					</div>
+				</div>
+			</div>
+
+	);
 }
 
 export default Menu;
